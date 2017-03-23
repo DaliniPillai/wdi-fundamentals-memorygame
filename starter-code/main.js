73d9d621aa096b892399d58b681b277f6cb65376
Dalini Pillai <dalinipillai@Fizals-MacBook.home>
2017-03-23 00:29:37 -0400

    pop up message

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,10 @@
-console.log("JS file is connected to HTML! Woo!")
-
-
 var cardOne = "queen"; //assigned var cardOne and two to the string "queen"
 var cardTwo = "queen";
 var cardThree = "king";//assigned var cardThree and Four to the string "king"
 var cardfour = "king";
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
