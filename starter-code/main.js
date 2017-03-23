29c6c0db163a348a918e34fe6aad8c8d79998bf7
Dalini Pillai <dalinipillai@Fizals-MacBook.home>
2017-03-22 22:17:55 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
+
+
+var cardOne = "queen"; //assigned var cardOne and two to the string "queen"
+var cardTwo = "queen";
+var cardThree = "king";//assigned var cardThree and Four to the string "king"
+var cardfour = "king";
